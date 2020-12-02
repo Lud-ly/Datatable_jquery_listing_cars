b658344f52eb2d4bdd52ee9ef2bf5d1f461e973b
Lud-ly <Ludom82@gmail.com>
2020-12-02 19:38:49 +0100

    initial commit

---
+++ js/carsList.js
@@ -0,0 +1,77 @@
+var aOfCars = [];
+aOfCars[0] = [];
+aOfCars[0]["annee"] = "2006";
+aOfCars[0]["marque"] = "Renault";
+aOfCars[0]["modele"] = "Clio rs";
+aOfCars[0]["motorisation"] = "2.0L 16S";
+aOfCars[0]["cv"] = "200";
+
+aOfCars[1] = [];
+aOfCars[1]["annee"] = "2018";
+aOfCars[1]["marque"] = "Bmw";
+aOfCars[1]["modele"] = "i3";
+aOfCars[1]["motorisation"] = "Electrique";
+aOfCars[1]["cv"] = "150";
+
+aOfCars[2] = [];
+aOfCars[2]["annee"] = "2019";
+aOfCars[2]["marque"] = "Porsche";
+aOfCars[2]["modele"] = "911 Carrera S";
+aOfCars[2]["motorisation"] = "3.0L V6";
+aOfCars[2]["cv"] = "450";
+
+aOfCars[3] = [];
+aOfCars[3]["annee"] = "1979";
+aOfCars[3]["marque"] = "Ford";
+aOfCars[3]["modele"] = "Shelby GT500";
+aOfCars[3]["motorisation"] = "5.2L V8";
+aOfCars[3]["cv"] = "800";
+
+aOfCars[4] = [];
+aOfCars[4]["annee"] = "2019";
+aOfCars[4]["marque"] = "Peugeot";
+aOfCars[4]["modele"] = "308 GTI";
+aOfCars[4]["motorisation"] = "2.0L 16s";
+aOfCars[4]["cv"] = "210";
+
+aOfCars[5] = [];
+aOfCars[5]["annee"] = "1965";
+aOfCars[5]["marque"] = "Citroen";
+aOfCars[5]["modele"] = "2cv";
+aOfCars[5]["motorisation"] = "1.2L";
+aOfCars[5]["cv"] = "40";
+
+aOfCars[6] = [];
+aOfCars[6]["annee"] = "2016";
+aOfCars[6]["marque"] = "Honda";
+aOfCars[6]["modele"] = "Civic";
+aOfCars[6]["motorisation"] = "Hybride";
+aOfCars[6]["cv"] = "110";
+
+aOfCars[7] = [];
+aOfCars[7]["annee"] = "2015";
+aOfCars[7]["marque"] = "Volkswagen";
+aOfCars[7]["modele"] = "Golf";
+aOfCars[7]["motorisation"] = "1.9Diesel";
+aOfCars[7]["cv"] = "90";
+
+aOfCars[8] = [];
+aOfCars[8]["annee"] = "2020";
+aOfCars[8]["marque"] = "Peugeot";
+aOfCars[8]["modele"] = "208";
+aOfCars[8]["motorisation"] = "Electrique";
+aOfCars[8]["cv"] = "130";
+
+aOfCars[9] = [];
+aOfCars[9]["annee"] = "2020";
+aOfCars[9]["marque"] = "Porsche";
+aOfCars[9]["modele"] = "Taycan";
+aOfCars[9]["motorisation"] = "Electrique";
+aOfCars[9]["cv"] = "760";
+
+aOfCars[10] = [];
+aOfCars[10]["annee"] = "2020";
+aOfCars[10]["marque"] = "Tesla";
+aOfCars[10]["modele"] = "Model 3";
+aOfCars[10]["motorisation"] = "Electrique";
+aOfCars[10]["cv"] = "230";
---
+++ js/main.js
@@ -0,0 +1,182 @@
+function constructTable() {
+    var i;
+
+    var sHTML = "";
+    sHTML += "<thead>";
+    sHTML += "<tr>";
+    sHTML += "<td>Année</td>";
+    sHTML += "<td>Marque</td>";
+    sHTML += "<td>Modèle</td>";
+    sHTML += "<td>Motorisation</td>";
+    sHTML += "<td>CV</td>";
+    sHTML += "<td>Editer</td>";
+    sHTML += "<td>Supprimer</td>";
+    sHTML += "</tr>";
+    sHTML += "</thead>";
+    sHTML += "<tbody>";
+
+    for (i = 0; i < aOfCars.length; i++) {
+        sHTML += "<tr>";
+        sHTML += "<td>" + aOfCars[i]["annee"] + "</td>";
+        sHTML += "<td>" + aOfCars[i]["marque"] + "</td>";
+        sHTML += "<td>" + aOfCars[i]["modele"] + "</td>";
+        sHTML += "<td>" + aOfCars[i]["motorisation"] + "</td>";
+        sHTML += "<td>" + aOfCars[i]["cv"] + "</td>";
+        sHTML += "<td class='icon' onClick=\"editcar(" + i + ")\"><img src='./img/edit_pencil.png'></td>";
+        sHTML += "<td  class='icon' onClick=\"deleteCar(" + i + ")\"><img src='./img/trash.png'></td>";
+        sHTML += "</tr>";
+    }
+
+    sHTML += "</tbody>";
+    $('#table_cars').html(sHTML);
+}
+
+function addCar() {
+    // Longueur actuel du tableau JS :
+    var iLongueur = aOfCars.length;
+
+    // ajout une voiture en fin de tableau JS
+    aOfCars[iLongueur] = [];
+    aOfCars[iLongueur]["annee"] = $('#annee').val();
+    aOfCars[iLongueur]["marque"] = $('#marque').val();
+    aOfCars[iLongueur]["modele"] = $('#modele').val();
+    aOfCars[iLongueur]["motorisation"] = $('#motorisation').val();
+    aOfCars[iLongueur]["cv"] = $('#cv').val();
+
+    // Reconstruire le tableau HTML
+    rebuildDatable();
+
+    // Vider le formulaire
+    clearForm();
+}
+
+function rebuildDatable() {
+    $('#table_cars').html("");
+    tables.clear();
+    tables.destroy();
+    constructTable();
+    tables = $('#table_cars').DataTable(configuration);
+}
+//Fonction Vider le formulaire
+function clearForm() {
+    $("input").val(" ");
+
+}
+
+function majCar() {
+    // Modifier les éléments du tableau dont l'indice est iIndiceEditEnCours
+    // ajout la modif à l'indice iIndiceEditEnCours
+    aOfCars[iIndiceEditEnCours]["annee"] = $('#annee').val();
+    aOfCars[iIndiceEditEnCours]["marque"] = $('#marque').val();
+    aOfCars[iIndiceEditEnCours]["modele"] = $('#modele').val();
+    aOfCars[iIndiceEditEnCours]["motorisation"] = $('#motorisation').val();
+    aOfCars[iIndiceEditEnCours]["cv"] = $('#cv').val();
+
+    // Reconstruire le tableau HTML
+    rebuildDatable();
+    // Vider le formulaire
+    clearForm();
+    // Remettre mes boutons correctement
+    // Montrer le bouton ajouter
+    $(".btn_action").show();
+    // Cacher le bouton modifier
+    $(".btn_modif").removeClass('show');
+    // Cacher le bouton annuler
+    $(".btn_save").removeClass('show');
+}
+
+function deleteCar(iIndiceEditEnCours) {
+    // Supprimer les éléments du tableau dont l'indice est iIndiceEditEnCours
+    //console.log(aOfCars[iIndiceEditEnCours]);
+    alert("Voulez vous vraiment supprimez " + aOfCars[iIndiceEditEnCours]["marque"] + " " + aOfCars[iIndiceEditEnCours]["modele"] + " ?");
+    aOfCars.splice(iIndiceEditEnCours, 1);
+
+    rebuildDatable();
+}
+//Annuler modif
+function annulCar() {
+    clearForm();
+    // Reconstruire le tableau HTML
+    rebuildDatable();
+
+    // Remettre mes boutons correctement
+    // Montrer le bouton ajouter
+    $(".btn_action").show();
+    // Cacher le bouton modifier
+    $(".btn_modif").removeClass("show");
+    // Cacher le bouton annuler
+    $(".btn_save").removeClass("show");
+}
+
+var iIndiceEditEnCours;
+//Modifier voiture
+function editcar(iIndiceEdit) {
+    alert("Modifier " + aOfCars[iIndiceEdit]["marque"] + " " + aOfCars[iIndiceEdit]["modele"] + " ?");
+    iIndiceEditEnCours = iIndiceEdit;
+    // J'édite la voiture en question
+    $('#annee').val(aOfCars[iIndiceEdit]["annee"]);
+    $('#marque').val(aOfCars[iIndiceEdit]["marque"]);
+    $('#modele').val(aOfCars[iIndiceEdit]["modele"]);
+    $('#motorisation').val(aOfCars[iIndiceEdit]["motorisation"]);
+    $('#cv').val(aOfCars[iIndiceEdit]["cv"]);
+
+    // cacher le bouton ajouter
+    $(".btn_action").hide();
+    // Montrer le bouton modifier
+    $(".btn_modif").addClass("show");
+    // Montrer le bouton annuler
+    $(".btn_save").addClass("show");
+}
+
+// CONFIGURATION DATATABLE
+const configuration = {
+    "stateSave": false,
+    "order": [[1, "asc"]],
+    "pagingType": "simple_numbers",
+    "searching": true,
+    "lengthMenu": [[5, 10, 25, 50, 100, -1], ["Cinq", "Dix", "Vingt cinq", "Cinquante", "Cent", "Ze total stp"]],
+    "language": {
+        "info": "Véhicules _START_ à _END_ sur _TOTAL_ sélectionnés",
+        "emptyTable": "Aucun véhicule",
+        "lengthMenu": "_MENU_ Véhicules par page",
+        "search": "Rechercher : ",
+        "zeroRecords": "Aucun résultat de recherche",
+        "paginate": {
+            "previous": "Précédent",
+            "next": "Suivant"
+        },
+        "sInfoFiltered": "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+        "sInfoEmpty": "Voitures 0 à 0 sur 0 sélectionné",
+    },
+    "columns": [
+        {
+            "orderable": true
+        },
+        {
+            "orderable": true
+        },
+        {
+            "orderable": true
+        },
+        {
+            "orderable": true
+        },
+        {
+            "orderable": true
+        },
+        {
+            "orderable": false
+        },
+        {
+            "orderable": false
+        }
+    ],
+    'retrieve': true
+};
+
+var tables;
+$(document).ready(function () {
+    constructTable();
+    // INIT DATATABLE
+    tables = $('#table_cars').DataTable(configuration);
+});
